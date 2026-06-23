61c46fba947b102d457cc24dbe7220ceb9f94b6b
Ceph <gwethcephas01@gmail.com>
2026-06-23 15:17:20 +0300

    feat: add TransactionEntity and RawTransaction mappers

---
+++ core/src/main/java/com/cephcoding/core/data/mapper/Mapper.kt
@@ -0,0 +1,38 @@
+package com.cephcoding.core.data.mapper
+
+import com.cephcoding.core.data.database.TransactionEntity
+import com.cephcoding.core.domain.model.ExpenseCategory
+import com.cephcoding.core.domain.model.RawTransaction
+import com.cephcoding.core.domain.model.TransactionType
+
+fun RawTransaction.toTransactionEntity(): TransactionEntity {
+    return TransactionEntity(
+        transactionId = this.transactionId,
+        amount = this.amount,
+        party = this.party,
+        type = this.type.name,
+        category = this.category?.name ?: ExpenseCategory.UNCATEGORIZED.name,
+        rawBody = this.rawBody
+    )
+}
+
+fun TransactionEntity.toRawTransaction(): RawTransaction {
+    return RawTransaction(
+        transactionId = this.transactionId,
+        amount = this.amount,
+        party = this.party,
+        type = try {
+            TransactionType.valueOf(this.type)
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+            TransactionType.UNKNOWN
+        },
+        category = try {
+            ExpenseCategory.valueOf(this.category)
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+            ExpenseCategory.UNCATEGORIZED
+        },
+        rawBody = this.rawBody
+    )
+}
