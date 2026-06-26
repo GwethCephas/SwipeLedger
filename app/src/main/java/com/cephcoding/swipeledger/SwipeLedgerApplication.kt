e57fdf6ee6de3d43a4c09b00dd18ca432b932424
Ceph <gwethcephas01@gmail.com>
2026-06-26 12:18:18 +0300

    feat: add dashboard module

--- app/src/main/java/com/cephcoding/swipeledger/SwipeLedgerApplication.kt
+++ app/src/main/java/com/cephcoding/swipeledger/SwipeLedgerApplication.kt
@@ -2,6 +2,7 @@ package com.cephcoding.swipeledger
 
 import android.app.Application
 import com.cephcoding.core.di.coreModule
+import com.cephcoding.feature.dashboard.di.dashboardModule
 import com.cephcoding.feature.sms.di.smsModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -13,6 +14,7 @@ class SwipeLedgerApplication : Application() {
             androidContext(this@SwipeLedgerApplication)
             modules(
                 smsModule,
+                dashboardModule,
                 coreModule
             )
         }
